c858fddf05258dc36e76f549bd1c5a017b5ffda1
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2022-12-10 23:49:55 -0300

    chore: base layer from maplibre and server running 3002 with tileset unified

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /* global maplibregl */ // Imported via <script> tag
 import maplibre3DTilesLayer from './maplibre-3d-tiles-layer/maplibre-3d-tiles-layer';
 
-const BASE_TILESET_URL = 'https://dami.loca.lt';
-const BUENOS_AIRES_TILESET_URL = `${BASE_TILESET_URL}/MESH_2021/L15/LR/6359_6158_-001_lv15_0_transform.json`;
+const BASE_TILESET_URL = 'http://localhost:3002';
+// const BUENOS_AIRES_TILESET_URL = `${BASE_TILESET_URL}/MESH_2021/L15/LR/6359_6158_-001_lv15_0_transform.json`;
+const BUENOS_AIRES_TILESET_URL = `${BASE_TILESET_URL}/tileset_unified.json`;
 
 // Load the maplibre map
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+  style: 'https://demotiles.maplibre.org/style.json',
   center: [-58.381507, -34.603344],
   zoom: 8.5,
   bearing: 0,
